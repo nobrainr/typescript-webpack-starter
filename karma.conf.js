20374e9309c0a7767ad5a482348106b1f401466d
Sami MADINI <sami.madini@gmail.com>
2017-02-01 18:35:46 +0100

    Update for kjhtml

--- karma.conf.js
+++ karma.conf.js
@@ -17,11 +17,23 @@ module.exports = function (config) {
       noInfo: true,
       stats: 'errors-only'
     },
-    reporters: ['progress', 'karma-typescript', 'coverage'],
+    reporters: ['progress', 'karma-typescript', 'kjhtml', 'coverage'],
     // optionally, configure the reporter
     coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
+      // specify a common output directory
+      dir: 'build/reports/coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
     },
     port: 9876,
     colors: true,
