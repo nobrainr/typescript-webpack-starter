aff34e0b30d664aae19c26aaac81471cbe628a38
Yann Renaudin <renaudin.yann@gmail.com>
2018-02-09 20:48:51 -0500

    fix(Test): Fix UTs coverage with Istanbul configuration

--- packages/cli/template/karma.conf.js
+++ packages/cli/template/karma.conf.js
@@ -1,10 +1,10 @@
-var webpackConf = require('./webpack.config.js');
-module.exports = function (config) {
+var webpackConf = require('./webpack.config.js')
+module.exports = function(config) {
   config.set({
-    basePath:'',
+    basePath: '',
     frameworks: ['jasmine'],
     files: [{ pattern: './tests/unit/spec-bundle.js', watched: false }],
-    preprocessors: { './tests/unit/spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+    preprocessors: { './tests/unit/spec-bundle.js': ['webpack', 'sourcemap'] },
     webpack: {
       module: webpackConf.module,
       resolve: webpackConf.resolve
@@ -13,31 +13,22 @@ module.exports = function (config) {
       noInfo: true,
       stats: 'errors-only'
     },
-    reporters: ['kjhtml', 'spec', 'coverage'],
-    // optionally, configure the reporter
-    coverageReporter: {
-      // specify a common output directory
-      dir: './tests/build/reports/coverage',
-      reporters: [
-        // reporters not supporting the `file` property
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' },
-        // reporters supporting the `file` property, use `subdir` to directly
-        // output them in the `dir` directory
-        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
-        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
-        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
-        { type: 'text', subdir: '.', file: 'text.txt' },
-        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
-      ]
-    },
+    reporters: ['spec', 'coverage-istanbul'],
     specReporter: {
-      maxLogLines: 5,         // limit number of lines logged per test 
-      suppressErrorSummary: true,  // do not print error summary 
-      suppressFailed: false,  // do not print information about failed tests 
-      suppressPassed: false,  // do not print information about passed tests 
-      suppressSkipped: true,  // do not print information about skipped tests 
-      showSpecTiming: false // print the time elapsed for each spec 
+      maxLogLines: 5, // limit number of lines logged per test
+      suppressErrorSummary: true, // do not print error summary
+      suppressFailed: false, // do not print information about failed tests
+      suppressPassed: false, // do not print information about passed tests
+      suppressSkipped: true, // do not print information about skipped tests
+      showSpecTiming: false // print the time elapsed for each spec
+    },
+    coverageIstanbulReporter: {
+      reports: ['html', 'lcov', 'text-summary'],
+      dir: './tests/coverage', // coverage results needs to be saved under coverage/
+      fixWebpackSourcePaths: true,
+      query: {
+        esModules: true
+      }
     },
     customLaunchers: {
       Chrome_travis_ci: {
@@ -52,10 +43,9 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: true,
     concurrency: Infinity
-  });
-
+  })
 
   if (process.env.TRAVIS) {
-    config.browsers = ['Chrome_travis_ci'];
+    config.browsers = ['Chrome_travis_ci']
   }
-};
+}
