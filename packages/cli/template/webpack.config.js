91a40c54099f6e22db1e1bab320ff9e22192387f
Yann Renaudin <renaudin.yann@gmail.com>
2018-02-09 20:48:17 -0500

    feat(Webpack): Add istanbul instrumenter loader configuration. File formatting

--- packages/cli/template/webpack.config.js
+++ packages/cli/template/webpack.config.js
@@ -1,55 +1,63 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DashboardPlugin = require("webpack-dashboard/plugin");
-const nodeEnv = process.env.NODE_ENV || "development";
-const isProd = nodeEnv === "production";
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+const nodeEnv = process.env.NODE_ENV || 'development'
+const isProd = nodeEnv === 'production'
 
 var config = {
-  devtool: isProd ? "hidden-source-map" : "source-map",
-  context: path.resolve("./src"),
+  devtool: isProd ? 'hidden-source-map' : 'source-map',
+  context: path.resolve('./src'),
   entry: {
-    app: "./index.ts",
-    vendor: "./vendor.ts"
+    app: './index.ts',
+    vendor: './vendor.ts'
   },
   output: {
-    path: path.resolve("./dist"),
-    filename: "[name].bundle.js",
-    sourceMapFilename: "[name].bundle.map",
-    devtoolModuleFilenameTemplate: function (info) {
-        return "file:///" + info.absoluteResourcePath;
+    path: path.resolve('./dist'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].bundle.map',
+    devtoolModuleFilenameTemplate: function(info) {
+      return 'file:///' + info.absoluteResourcePath
     }
   },
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.ts?$/,
-        exclude: ["node_modules"],
-        use: ["awesome-typescript-loader", "source-map-loader"]
+        exclude: ['node_modules'],
+        use: ['awesome-typescript-loader', 'source-map-loader']
       },
-      { test: /\.html$/, loader: "html-loader" },
-      { test: /\.css$/, loaders: ["style-loader", "css-loader"] }
+      {
+        test: /\.(js|ts)$/,
+        loader: 'istanbul-instrumenter-loader',
+        exclude: [/\/node_modules\//],
+        query: {
+          esModules: true
+        }
+      },
+      { test: /\.html$/, loader: 'html-loader' },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
     ]
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: ['.ts', '.js']
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
+      'process.env': {
         // eslint-disable-line quote-props
         NODE_ENV: JSON.stringify(nodeEnv)
       }
     }),
     new HtmlWebpackPlugin({
-      title: "Typescript Webpack Starter",
-      template: "!!ejs-loader!src/index.html"
+      title: 'Typescript Webpack Starter',
+      template: '!!ejs-loader!src/index.html'
     }),
     new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
+      name: 'vendor',
       minChunks: Infinity,
-      filename: "vendor.bundle.js"
+      filename: 'vendor.bundle.js'
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false },
@@ -67,11 +75,11 @@ var config = {
     })
   ],
   devServer: {
-    contentBase: path.join(__dirname, "dist/"),
+    contentBase: path.join(__dirname, 'dist/'),
     compress: true,
     port: 3000,
     hot: true
   }
-};
+}
 
-module.exports = config;
+module.exports = config
