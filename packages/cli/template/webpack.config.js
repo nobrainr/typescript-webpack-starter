3527c445871c0f84264e56318b2555ef94684bc9
Yann Renaudin <renaudin.yann@gmail.com>
2018-08-21 18:44:17 -0400

    feat: Update webpack configuration to be compliant with version 4

--- packages/cli/template/webpack.config.js
+++ packages/cli/template/webpack.config.js
@@ -1,25 +1,10 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
 
 const plugins = [
-  new UglifyJsPlugin({
-    parallel: true,
-    uglifyOptions: {
-      ie8: false,
-      ecma: 6,
-      warnings: true,
-      mangle: isProd, // debug false
-      output: {
-        comments: false,
-        beautify: !isProd // debug true
-      }
-    },
-    sourceMap: true
-  }),
   new webpack.DefinePlugin({
     'process.env': {
       // eslint-disable-line quote-props
@@ -44,22 +29,11 @@ var config = {
   devtool: isProd ? 'hidden-source-map' : 'source-map',
   context: path.resolve('./src'),
   entry: {
-    app: './index.ts',
-    vendor: './vendor.ts'
+    app: './index.ts'
   },
   output: {
     path: path.resolve('./dist'),
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].bundle.map',
-    devtoolModuleFilenameTemplate: function(info) {
-      return 'file:///' + info.absoluteResourcePath;
-    }
-  },
-  optimization: {
-    splitChunks: {
-      // include all types of chunks
-      chunks: 'all'
-    }
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
