e1e67b2acd7db03876326533d9c880222b1db0a4
Yann Renaudin <renaudin.yann@gmail.com>
2018-08-22 01:01:37 -0400

    chore: typo

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-git --global config user.email "renaudin.yann@gmail.com"
-git --global config user.name "Yann Renaudin from CircleCI"
+git config --global  user.email "renaudin.yann@gmail.com"
+git config --global  user.name "Yann Renaudin from CircleCI"
