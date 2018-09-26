2ddbda5c10d8dae6adc326060e88ba39e405bfe8
nrgwsth <anuragawasthi13@gmail.com>
2018-09-27 01:07:17 +0530

    Update webpack.config.js
    webpack gives error that `['node_modules']` is not a valid exclude option. Use regex `/node_modules/`.

--- packages/cli/template/webpack.config.js
+++ packages/cli/template/webpack.config.js
@@ -39,7 +39,7 @@ var config = {
       {
         enforce: 'pre',
         test: /\.tsx?$/,
-        exclude: ['node_modules'],
+        exclude: [/\/node_modules\//],
         use: ['awesome-typescript-loader', 'source-map-loader']
       },
       !isProd
